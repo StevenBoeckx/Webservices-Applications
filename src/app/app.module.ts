b353d8a3be1bdebcb6226485c724a147b899b125
StevenBoeckx <steven.boeckx1@gmail.com>
2017-11-11 11:19:15 +0100

    first commit
    Basic setup from website with angular started frome demo project
    added:
    - side bar
    - ng2-draganddrop

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { Ng2DragDropModule } from 'ng2-drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+      Ng2DragDropModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
