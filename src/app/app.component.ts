6bb67c9a9b8916d58c3c855db152a292b7d87701
StevenBoeckx <steven.boeckx1@gmail.com>
2017-11-12 10:18:04 +0100

    fixed some compile problems (wrong path) also added input on sensorIdCart component so correct ID could be given from parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    sensors = [ 1 , 2 , 3 , 4 ];
+    sensors = [ 1 , 4 , 23 , 40 ];
   title = 'app';
 }
--- src/app/sensor-id-cart/sensor-id-cart.component.ts
+++ src/app/sensor-id-cart/sensor-id-cart.component.ts
@@ -1,41 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-sensor-id-cart',
-  templateUrl: './sensor-id-cart.component.html',
-  styleUrls: ['./sensor-id-cart.component.css']
+    selector: 'app-sensor-id-cart',
+    templateUrl: './sensor-id-cart.component.html',
+    styleUrls: ['./sensor-id-cart.component.css']
 })
 export class SensorIdCartComponent implements OnInit {
+    @Input() ID: number;
+    lastUpdated = 25;
+    batterylevel = 50;
+    statusUpdating;
+    statusBattery;
 
-  lastUpdated = 25;
-  batterylevel = 50;
-  ID=1;
-  statusUpdating;
-  statusBattery;
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-      if ( this.lastUpdated > 20 ) {
-          this.statusUpdating = 'alert alert-danger';
-      }
-      else if (this.lastUpdated > 10 && this.lastUpdated < 20) {
+    ngOnInit() {
+        if (this.lastUpdated > 20) {
+            this.statusUpdating = 'alert alert-danger';
+        }
+        else if (this.lastUpdated > 10 && this.lastUpdated < 20) {
 
-          this.statusUpdating = 'alert alert-warning';
-      }
-      else {
-          this.statusUpdating = 'alert alert-success';
-      }
+            this.statusUpdating = 'alert alert-warning';
+        }
+        else {
+            this.statusUpdating = 'alert alert-success';
+        }
 
-      if ( this.batterylevel > 30 ) {
-          this.statusBattery = 'alert alert-success';
-      }
-      else if (this.batterylevel > 10 && this.batterylevel < 30) {
+        if (this.batterylevel > 30) {
+            this.statusBattery = 'alert alert-success';
+        }
+        else if (this.batterylevel > 10 && this.batterylevel < 30) {
 
-          this.statusBattery = 'alert alert-warning';
-      }
-      else {
-          this.statusBattery = 'alert alert-danger';
-      }
-  }
+            this.statusBattery = 'alert alert-warning';
+        }
+        else {
+            this.statusBattery = 'alert alert-danger';
+        }
+    }
 
 }
