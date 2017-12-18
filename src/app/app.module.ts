6cb9d50261b85bf9d4a111c402974ccb3bc4ab80
StevenBoeckx <steven.boeckx1@gmail.com>
2017-12-18 21:47:07 +0100

    added routes at front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import {SensorIdCartComponent} from './sensor-id-cart/sensor-id-cart.component';
 import {LogWindowComponent} from './log-window/log-window.component';
 import {LogScreenComponent} from './log-screen/log-screen.component';
 import {ChartsModule} from 'ng2-charts';
+import {RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const appRoutes: Routes = [{
+    path: '', component: HomeComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'sensorLog', component: LogWindowComponent},
+];
 
 @NgModule({
     declarations: [
@@ -13,11 +24,15 @@ import {ChartsModule} from 'ng2-charts';
         SensorIdCartComponent,
         LogWindowComponent,
         LogScreenComponent,
+        HomeComponent,
+        LoginComponent,
+        SignupComponent,
     ],
     imports: [
         BrowserModule,
         Ng2DragDropModule.forRoot(),
-        ChartsModule
+        ChartsModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [],
     bootstrap: [AppComponent]
