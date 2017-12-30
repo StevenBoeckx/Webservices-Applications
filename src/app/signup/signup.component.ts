2f81b0587ff0652f799d4ca64a8f3cd51ae0769b
StevenBoeckx <steven.boeckx1@gmail.com>
2017-12-30 19:04:50 +0100

    added signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  email='';
+  password='';
+  name='';
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   ngOnInit() {
+  }
+  signupNewuser()
+  {
+  this.http.post('http://localhost/api/signup'
+          , JSON.stringify({email: this.email , password: this.password, name:this.name}), {headers: new HttpHeaders().set('content-Type', 'application/json')}
+      ).subscribe(
+          (data) => {
+              let correctRedirectPath = '/sensorLog';
+              this.auth.login(this.email, this.password, correctRedirectPath);
+          },
+          (err: HttpErrorResponse) => {
+              console.log(err);
+          },
+          () => {
+          }
+  );
+
   }
 
 }
